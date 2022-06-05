7bcf41dc604605839d49817fb9d5ac078d5531cc
Shubh <shubhamshukla@rupeeseed.com>
2022-06-05 23:46:18 +0530

    styles changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NumberScrollComponent } from './number-scroll/number-scroll.component';
+import { NumberColumnComponent } from './number-scroll/number-column/number-column.component';
+import { DecimalColumnComponent } from './number-scroll/decimal-column/decimal-column.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberScrollComponent,
+    NumberColumnComponent,
+    DecimalColumnComponent
   ],
   imports: [
     BrowserModule
